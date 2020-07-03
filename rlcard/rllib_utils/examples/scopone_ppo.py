c35e325e4e98bc8b50408cb53d075ad3159b0f2b
chiappal <alberto.chiappa@schindler.com>
2020-07-03 14:06:35 +0200

    Make scopone_ppo train a multiple agents

--- rlcard/rllib_utils/examples/scopone_ppo.py
+++ rlcard/rllib_utils/examples/scopone_ppo.py
@@ -1,5 +1,5 @@
 import ray
-from ray.rllib.agents.ppo import PPOTrainer
+from ray.rllib.agents.ppo import PPOTrainer, PPOTFPolicy
 from ray.rllib.models import ModelCatalog
 from ray.tune import register_env
 
@@ -8,20 +8,50 @@ from rlcard.rllib_utils.rlcard_wrapper import RLCardWrapper
 
 rlcard_env_id = 'scopone'
 
-register_env("ParametricScopone", lambda _: RLCardWrapper(rlcard_env_id=rlcard_env_id))
+
+scopone_env = RLCardWrapper(rlcard_env_id=rlcard_env_id)
+register_env("ParametricScopone", lambda _: scopone_env)
 ModelCatalog.register_custom_model("parametric_model_tf", ParametricActionsModel)
 
 ray.init(num_cpus=4)
 
-ppo_trainer_config = {
+ppo_policy_config = {
     "env": "ParametricScopone",
     "model": {
         "custom_model": "parametric_model_tf",
+        "fcnet_hiddens": [512, 256, 128],
+        "use_lstm": True,
     },
 }
+
+policies = {
+    "ppo_policy_albi": (PPOTFPolicy,
+                        scopone_env.observation_space,
+                        scopone_env.action_space,
+                        ppo_policy_config),
+    "ppo_policy_nico": (PPOTFPolicy,
+                        scopone_env.observation_space,
+                        scopone_env.action_space,
+                        ppo_policy_config)
+}
+
+ppo_trainer_config = {
+    "env": "ParametricScopone",
+    "multiagent": {
+        "policies_to_train": ["ppo_policy_albi", "ppo_policy_nico"],
+        "policies": policies,
+        "policy_mapping_fn": lambda agent_id: "ppo_policy_albi" if agent_id in ("player_1", "player_2") else "ppo_policy_nico",
+    },
+    "observation_filter": "NoFilter"
+}
+
 trainer = PPOTrainer(config=ppo_trainer_config)
+trainer.get_policy("ppo_policy_albi").model.base_model.summary()
+trainer.get_policy("ppo_policy_nico").model.base_model.summary()
+
 for i in range(5):
     res = trainer.train()
-    print("Iteration {}. episode_reward_mean: {}".format(i, res['episode_reward_mean']))
+    print("Iteration {}. policy_reward_mean: {}".format(i, res['policy_reward_mean']))
+    trainer.save()
 
 print('Training finished, check the results in ~/ray_results/<dir>/')
