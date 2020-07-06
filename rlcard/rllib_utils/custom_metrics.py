767af9acbd6e7628e63abb0df9409c856ecf85a2
chiappal <alberto.chiappa@schindler.com>
2020-07-06 09:24:07 +0200

    Remove commented lines from the custom metrics

--- rlcard/rllib_utils/custom_metrics.py
+++ rlcard/rllib_utils/custom_metrics.py
@@ -15,7 +15,6 @@ class PlayerScoreCallbacks(DefaultCallbacks):
     def on_episode_start(self, worker: RolloutWorker, base_env: BaseEnv,
                          policies: Dict[str, Policy],
                          episode: MultiAgentEpisode, **kwargs):
-        # print("episode {} started".format(episode.episode_id))
         pass
 
     def on_episode_step(self, worker: RolloutWorker, base_env: BaseEnv,
@@ -25,20 +24,15 @@ class PlayerScoreCallbacks(DefaultCallbacks):
     def on_episode_end(self, worker: RolloutWorker, base_env: BaseEnv,
                        policies: Dict[str, Policy], episode: MultiAgentEpisode,
                        **kwargs):
-        # player_scores = base_env.rlcard_env.get_payoffs()
         player_scores_raw = episode.agent_rewards
         player_scores = {f"{p}_score": v for (p, _), v in player_scores_raw.items()}
         episode.custom_metrics.update(player_scores)
-        print(dict(player_scores))
 
     def on_sample_end(self, worker: RolloutWorker, samples: SampleBatch,
                       **kwargs):
-        # print("returned sample batch of size {}".format(samples.count))
         pass
 
     def on_train_result(self, trainer, result: dict, **kwargs):
-        # print("trainer.train() result: {} -> {} episodes".format(
-        #     trainer, result["episodes_this_iter"]))
         # you can mutate the result dict to add new fields to return
         result["callback_ok"] = True
 
@@ -47,8 +41,4 @@ class PlayerScoreCallbacks(DefaultCallbacks):
             agent_id: str, policy_id: str, policies: Dict[str, Policy],
             postprocessed_batch: SampleBatch,
             original_batches: Dict[str, SampleBatch], **kwargs):
-        # print("postprocessed {} steps".format(postprocessed_batch.count))
-        # if "num_batches" not in episode.custom_metrics:
-        #     episode.custom_metrics["num_batches"] = 0
-        # episode.custom_metrics["num_batches"] += 1
         pass
