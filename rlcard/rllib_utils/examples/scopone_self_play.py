14a03267f5f55fbe6d60b86faead7621140da3e6
chiappal <alberto.chiappa@schindler.com>
2020-07-11 21:35:29 +0200

    Tidy the scopone self-play file

--- rlcard/rllib_utils/examples/scopone_self_play.py
+++ rlcard/rllib_utils/examples/scopone_self_play.py
@@ -14,32 +14,36 @@ from ray.tune.registry import register_env
 import time
 
 rlcard_env_id = 'scopone'
+checkpoint_path = r"C:\Users\chiappal\ray_results\PPO_scopone_2020-07-07_23-25-17_training_2\checkpoint_5202\checkpoint-5202"
+restore_checkpoint = True
+num_iter = 10000
+checkpoint_every = 200
+eval_every = 10
 
 # Register env and model to be used by rllib
-RLCardWrapped = lambda _: RLCardWrapper(rlcard_env_id=rlcard_env_id)
-register_env(rlcard_env_id, RLCardWrapped)
+scopone_env = RLCardWrapper(rlcard_env_id=rlcard_env_id)
+register_env(rlcard_env_id, lambda _: scopone_env)
 ModelCatalog.register_custom_model("parametric_model_tf", ParametricActionsModel)
 
 # Initialize ray
-# ray.init(num_cpus=4, num_gpus=1)
-ray.init(num_cpus=4)
-#
+ray.init(num_cpus=4, num_gpus=0)
+
 # Define the policies
-ppo_trainer_config = {
+ppo_policy_config = {
     # "env": rlcard_env_id,
     "model": {
         "custom_model": "parametric_model_tf",
+        "fcnet_hiddens": [256, 256, 256],
     },
 }
-env_tmp = RLCardWrapped(None)
 policies = {
     "ppo_policy": (PPOTFPolicy,
-                   env_tmp.observation_space,
-                   env_tmp.action_space,
-                   ppo_trainer_config),
+                   scopone_env.observation_space,
+                   scopone_env.action_space,
+                   ppo_policy_config),
     "rand_policy": (RandomPolicy,
-                    env_tmp.observation_space,
-                    env_tmp.action_space,
+                    scopone_env.observation_space,
+                    scopone_env.action_space,
                     {}),
 }
 
@@ -51,7 +55,7 @@ trainer_eval = PPOTrainer(config={
         "policies": policies,
         "policy_mapping_fn": lambda agent_id: "ppo_policy" if agent_id in ("player_1", "player_3") else "rand_policy",
     },
-    # "num_gpus": 0.5,
+    # "num_gpus": 0,
     "callbacks": PlayerScoreCallbacks
 })
 
@@ -62,22 +66,27 @@ trainer = PPOTrainer(config={
         "policies": policies,
         "policy_mapping_fn": lambda agent_id: "ppo_policy",
     },
-    # "num_gpus": 0.5,
+    # "num_gpus": 0,
     # "num_gpus_per_worker": 0,
     "callbacks": PlayerScoreCallbacks
 })
 
+if restore_checkpoint:
+    trainer.restore(checkpoint_path)
+
 start = time.time()
-for i in range(10):
-    # trainer.set_weights(trainer_eval.get_weights(["ppo_policy"]))
-    trainer.train()
 
-    trainer_eval.set_weights(trainer.get_weights(["ppo_policy"]))
-    res = trainer_eval.train()
-
-    print(res)
-    policy_rewards = sorted(['{}: {}'.format(k, v) for k, v in res['policy_reward_mean'].items()])
-    print("Iteration {}. policy_reward_mean: {}".format(i, policy_rewards))
+try:
+    for i in range(num_iter):
+        res = trainer.train()
+        print("Iteration {}. policy result: {}".format(i, res))
+        if i % eval_every == 0:
+            trainer_eval.set_weights(trainer.get_weights(["ppo_policy"]))
+            res = trainer_eval.train()
+        if i % checkpoint_every == 0:
+            trainer.save()
+except:
+    trainer.save()
 
 stop = time.time()
 train_duration = time.strftime('%H:%M:%S', time.gmtime(stop-start))
